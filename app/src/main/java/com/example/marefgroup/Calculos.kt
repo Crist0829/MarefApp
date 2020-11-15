1da90f79639a87cc9e5726d3701d215f1d3de86a
Crist0829 <bqfidel16@gmail.com>
2020-11-15 20:42:33 -0300

    Entregable número 1 versión final, corregí todo lo que me pidieron y fijé la estructura

---
+++ app/src/main/java/com/example/marefgroup/Calculos.kt
@@ -0,0 +1,11 @@
+package com.example.marefgroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculos)
+    }
+}
